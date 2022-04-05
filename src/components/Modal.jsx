fad94c0c953b23ad481a33cffa7a36578c174369
Jhan Bayer <jhanbayer@gmail.com>
2022-04-05 12:41:36 -0400

    UI formulario adding

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -18,6 +18,36 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
         className={`formulario ${animarModal ? "animar" : "cerrar"}`}
       >
         <legend>Nuevo Gasto</legend>
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Añade el Nombre del Gasto"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            id="cantidad"
+            type="text"
+            placeholder="Añade la cantidad del Gasto: ej: 300"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="categoria">Categoria</label>
+          <select name="" id="categoria">
+            <option value="">-- Seleccione--</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+        <input type="submit" value="AÑADIR GASTO" />
       </form>
     </div>
   );
